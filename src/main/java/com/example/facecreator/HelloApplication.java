60e135f6b1288ba03e56a287f5dbf21ae67338e8
Simon Ørskov Dieckmann Pedersen <simonorskov@gmail.com>
2021-09-09 09:58:50 +0200

    inserted code from behu gitbook

---
+++ src/main/java/com/example/facecreator/HelloApplication.java
@@ -0,0 +1,60 @@
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class HelloApplication extends Application {
+    static int width = 600;
+    static int height = 600;
+    static Canvas canvas = new Canvas(width, height);
+    static GraphicsContext gc = canvas.getGraphicsContext2D();
+
+
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+
+        Group root = new Group();
+        Scene scene = new Scene(root, width, height);
+        stage.setTitle("Face");
+        stage.setScene(scene);
+
+        gc.clearRect(0, 0, width, height);
+        root.getChildren().add(canvas);
+
+        drawPrimitiveFace();
+
+        stage.show();
+    }
+
+    public static void drawPrimitiveFace() {
+        drawShape();
+        drawMouth(50);
+        drawEyes();
+    }
+
+    public static void drawShape() {
+        gc.strokeOval(150, 150, 300, 300);
+    }
+
+    public static void drawMouth(int mouthSize) {
+        gc.setFill(Color.BLACK);
+        gc.fillOval(280,370,mouthSize,mouthSize);
+    }
+
+    public static void drawEyes() {
+        gc.setFill(Color.BLACK);
+        gc.fillOval(220,220,20,20);
+        gc.fillOval(360,220,20,20);
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
